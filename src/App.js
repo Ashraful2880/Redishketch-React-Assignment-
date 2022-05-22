5f9cc4a82c57c9867169ffa070e6944012b1620b
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-22 16:43:32 +0600

    Basic Routes Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Footer from './Components/Shared/Footer/Footer';
+import Header from './Components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      Welcome Here
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            This is Home
+        <div style={{ minHeight: "60vh" }}>
+            This is Bannar Area
         </div>
     );
 };
